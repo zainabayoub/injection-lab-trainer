c838cb4ee439e6ad3fc1e213154d05b433ab749e
zainabayoub <zainabayoub96@gmail.com>
2026-03-17 20:49:34 -0400

    Create scenarios.js

---
+++ scenarios.js
@@ -0,0 +1,48 @@
+const scenarios = [
+
+{
+medication:"Dexamethasone",
+order:"Dexamethasone 6 mg IM",
+route:"IM",
+concentration:"10 mg/mL",
+correctDose:"0.6 mL",
+vials:["Dexamethasone","Heparin","Insulin","PPD"]
+},
+
+{
+medication:"Ketorolac",
+order:"Ketorolac 30 mg IM",
+route:"IM",
+concentration:"30 mg/mL",
+correctDose:"1 mL",
+vials:["Ketorolac","Vitamin B12","Heparin","Insulin"]
+},
+
+{
+medication:"Heparin",
+order:"Heparin 5000 units SQ",
+route:"SQ",
+concentration:"5000 units/mL",
+correctDose:"1 mL",
+vials:["Heparin","Dexamethasone","PPD","Ketorolac"]
+},
+
+{
+medication:"Regular Insulin",
+order:"Regular insulin 10 units SQ",
+route:"SQ",
+concentration:"U100",
+correctDose:"10 units",
+vials:["Regular Insulin","Heparin","Ketorolac","Vitamin B12"]
+},
+
+{
+medication:"PPD",
+order:"PPD 0.1 mL ID",
+route:"ID",
+concentration:"5TU / 0.1 mL",
+correctDose:"0.1 mL",
+vials:["PPD","Heparin","Dexamethasone","Insulin"]
+}
+
+]
